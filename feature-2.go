8d5ab3aa3257c9d8670df84fab93f5b15f09dc4e
H-Ngyen <hoangnguyennn0210@gmail.com>
2025-05-28 16:56:25 +0700

    Second change, edit readme

---
+++ feature-2.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func writeFeature2() {
+	fmt.Println("this is feature 2!")
+}
--- main.go
+++ main.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("test log from golang")
+	fmt.Printf("first change")
+
+	writeFeature2()
 }
